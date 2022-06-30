f5c4b36c634544d1f9ba3dc8b4e82a75f933766a
Singh Amisha <amishasingh9976@gmail.com>
2022-06-30 10:17:39 -0400

    Extract translated words from API

--- krapi.py
+++ krapi.py
@@ -3,30 +3,34 @@ import ssl
 import certifi
 from xml.etree import ElementTree as ET
 import xml.dom.minidom
+import sys
 
-file = open('word_lists/kr.txt', encoding ="utf-8")
-line = file.readline()
+file = open('word_lists/kr.txt', encoding ="UTF-8")
+line = file.readlines()
 newLine=""
 for i in line:
     if i!="\n":
         newLine+=i
-print(newLine)
 
-url = "https://krdict.korean.go.kr/api/search?certkey_no=3971&key=92425B7F6398BF034955A47FC88B8D6B&type_search=search&part=word&q="
-url2 = "&sort=popular&translated=y&trans_lang=1"
-# r = requests.get(url =url + "것" + url2, verify=False)
-# root = ET.fromstring(r.text)
+sys.stdout = open("krtranswords.txt", "w", encoding="utf-8")
+for word in newLine.split():
+    url = "https://krdict.korean.go.kr/api/search?certkey_no=3971&key=92425B7F6398BF034955A47FC88B8D6B&type_search=search&part=word&q="
+    url2 = "&sort=popular&translated=y&trans_lang=1"
+    response = requests.get(url = url + word + url2, verify=False)
+    root = ET.fromstring(response.text)
+    tree = ET.ElementTree(root)
+    tree.write("data.xml", encoding="UTF-8")
+    transWord = tree.find('.//trans_word').text
+    print(transWord)
+
+
+# url = "https://krdict.korean.go.kr/api/search?certkey_no=3971&key=92425B7F6398BF034955A47FC88B8D6B&type_search=search&part=word&q="
+# url2 = "&sort=popular&translated=y&trans_lang=1"
+# response = requests.get(url = url + newLine + url2, verify=False)
+# root = ET.fromstring(response.text)
 # tree = ET.ElementTree(root)
-# tree.write("data.xml")
-doc = xml.dom.minidom.parse("data.xml")
-channel = doc.firstChild
-# doc = xml.dom.minidom.parse(response);
-# print(doc.nodeName)
-
-# response.raw.decode_content = True
-# events = ElementTree.iterparse(response.raw)
-# for event, elem in events:
-#     print(elem)
-    
-#request2 = requests.get(url = "https://krdict.korean.go.kr/api/search?certkey_no=3971&key=92425B7F6398BF034955A47FC88B8D6B&type_search=search&part=word&q=%EB%82%98%EB%AC%B4&sort=dict", verify=False)
-
+# tree.write("data.xml", encoding="UTF-8")
+# transWord = tree.find('.//trans_word').text
+# transDef = tree.find('.//trans_dfn').text
+# print(transWord)
+# print(transDef)
