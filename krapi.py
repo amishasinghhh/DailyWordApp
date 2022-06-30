3ef289bb3dfe0ecaa1471ce45d01e008f48ac872
Singh Amisha <amishasingh9976@gmail.com>
2022-06-30 12:23:07 -0400

    Get KR definitions

--- krapi.py
+++ krapi.py
@@ -12,7 +12,7 @@ for i in line:
     if i!="\n":
         newLine+=i
 
-sys.stdout = open("krtranswords.txt", "w", encoding="utf-8")
+sys.stdout = open("krtranswords.txt", "a", encoding="utf-8")
 for word in newLine.split():
     url = "https://krdict.korean.go.kr/api/search?certkey_no=3971&key=92425B7F6398BF034955A47FC88B8D6B&type_search=search&part=word&q="
     url2 = "&sort=popular&translated=y&trans_lang=1"
@@ -21,7 +21,8 @@ for word in newLine.split():
     tree = ET.ElementTree(root)
     tree.write("data.xml", encoding="UTF-8")
     transWord = tree.find('.//trans_word').text
-    print(transWord)
+    #print(transWord)
+    #commented out to not overwrite krtranswords.txt
 
 
 # url = "https://krdict.korean.go.kr/api/search?certkey_no=3971&key=92425B7F6398BF034955A47FC88B8D6B&type_search=search&part=word&q="
