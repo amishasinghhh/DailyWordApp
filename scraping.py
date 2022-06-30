aa309aa65e3a7e55c6def4cb2837fb38e0ab5b02
Singh Amisha <amishasingh9976@gmail.com>
2022-06-29 21:18:47 -0400

    Add more Korean words

--- scraping.py
+++ scraping.py
@@ -35,35 +35,90 @@ while True:
     if not line:
         break
 
-#Scrape next 1000 words
-i = 2
-while i<7:
-    URL = "https://www.topikguide.com/6000-most-common-korean-words-2"
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, "html.parser")
-    results = soup.find(id='theme-content-section')
-    sys.stdout = open('intermediate.txt', "w", encoding="utf-8")
-    table = results.find("div", class_="section-content")
-    for x in table:
-        if x.find("td") :
-            x = x.text.strip()
-            print(x)
-    sys.stdout = open("intermediate2.txt", "w", encoding="utf-8")
-    sys.stdin = open (filePath, "r", encoding="utf-8")
-    while True:
-        line = sys.stdin.readline()
-        if len(line.strip())!=0:
-            print(line.strip())
-        if not line:
-            break  
-    sys.stdin = open("intermediate2.txt", "r", encoding="utf-8")
-    sys.stdout = open("krwords.txt", "w", encoding="utf-8")
-    count=0
-    while True:
-        line = sys.stdin.readline()
-        count+=1
-        if count%3==0:
-            print(line.strip())
-        if not line:
-            break
+#Scrape next 1000 words (1000-2000)
+URL = "https://www.topikguide.com/6000-most-common-korean-words-2"
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(id='theme-content-section')
+sys.stdout = open('intermediate.txt', "w", encoding="utf-8")
+table = results.find("div", class_="section-content")
+for x in table:
+    if x.find("td") :
+        x = x.text.strip()
+        print(x)
+sys.stdout = open("intermediate2.txt", "w", encoding="utf-8")
+sys.stdin = open (filePath, "r", encoding="utf-8")
+while True:
+    line = sys.stdin.readline()
+    if len(line.strip())!=0:
+        print(line.strip())
+    if not line:
+        break  
+sys.stdin = open("intermediate2.txt", "r", encoding="utf-8")
+sys.stdout = open("krwords.txt", "a", encoding="utf-8")
+count=0
+while True:
+    line = sys.stdin.readline()
+    count+=1
+    if count%3==0:
+        print(line.strip())
+    if not line:
+        break
 
+URL = "https://www.topikguide.com/6000-most-common-korean-words-3"
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(id='theme-content-section')
+sys.stdout = open('intermediate.txt', "w", encoding="utf-8")
+table = results.find("div", class_="section-content")
+for x in table:
+    if x.find("td") :
+        x = x.text.strip()
+        print(x)
+sys.stdout = open("intermediate2.txt", "w", encoding="utf-8")
+sys.stdin = open (filePath, "r", encoding="utf-8")
+while True:
+    line = sys.stdin.readline()
+    if len(line.strip())!=0:
+        print(line.strip())
+    if not line:
+        break  
+sys.stdin = open("intermediate2.txt", "r", encoding="utf-8")
+sys.stdout = open("krwords.txt", "a", encoding="utf-8")
+count=0
+while True:
+    line = sys.stdin.readline()
+    count+=1
+    if count%3==0:
+        print(line.strip())
+    if not line:
+        break
+
+URL = "https://www.topikguide.com/6000-most-common-korean-words-4"
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(id='theme-content-section')
+sys.stdout = open('intermediate.txt', "w", encoding="utf-8")
+table = results.find("div", class_="section-content")
+for x in table:
+    if x.find("td") :
+        x = x.text.strip()
+        print(x)
+sys.stdout = open("intermediate2.txt", "w", encoding="utf-8")
+sys.stdin = open (filePath, "r", encoding="utf-8")
+while True:
+    line = sys.stdin.readline()
+    if len(line.strip())!=0:
+        print(line.strip())
+    if not line:
+        break  
+sys.stdin = open("intermediate2.txt", "r", encoding="utf-8")
+sys.stdout = open("krwords.txt", "a", encoding="utf-8")
+count=0
+while True:
+    line = sys.stdin.readline()
+    count+=1
+    if count%3==0:
+        print(line.strip())
+    if not line:
+        break
