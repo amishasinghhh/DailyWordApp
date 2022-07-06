338552c0b277e1e646515fa60c6afe665b696794
Singh Amisha <amishasingh9976@gmail.com>
2022-07-05 23:42:31 -0400

    Korean information from API

--- kr-api.py
+++ kr-api.py
@@ -5,42 +5,57 @@ from xml.etree import ElementTree as ET
 import xml.dom.minidom
 import sys
 
-sys.stdin = open("kr_partsofspeech-kr.txt", "r", encoding="utf-8")
-sys.stdout = open("kr_partsofspeech-en.txt", "w", encoding="utf-8")
+## Commented out to prevent overwriting
+## Get example sentences for words from API
 
-line = sys.stdin.readlines()
-newLine=""
-for i in line:
-    if i!="\n":
-        newLine+=i
+# sys.stdin = open ('kr.txt', "r", encoding='utf-8')
+# sys.stdout = open ('kr_examples.txt', "w", encoding="utf-8")
 
-poseng=""
-for pos in newLine.splitlines():
-    if pos=="전체":
-        poseng="whole"
-    elif pos=="명사" or pos=="의존 명사":
-        poseng="noun"
-    elif pos=="대명사":
-        poseng="pronoun"
-    elif pos=="수사":
-        poseng="number"
-    elif pos=="조사":
-        poseng="article"
-    elif pos=="동사":
-        poseng="verb"
-    elif pos=="형용사" or pos=="관형사" or pos=="보조 형용사":
-        poseng="adjective"
-    elif pos=="부사":
-        poseng="adverb"
-    elif pos=="감탄사":
-        poseng="interjection"
-    elif pos=="접사":
-        poseng=="affix"
-    elif pos=="어미":
-        poseng="ending"
-    elif pos=="품사 없음":
-        poseng=="no part of speech"
-    print(poseng)
+# for line in sys.stdin:
+#   output=""
+#   for x in line.split():
+#     if x!="  ":
+#       output+=x+" "
+#   print(output)
+
+## Commented out to prevent overwriting
+## Convert Korean part of speech words to English
+# sys.stdin = open("kr_partsofspeech-kr.txt", "r", encoding="utf-8")
+# sys.stdout = open("kr_partsofspeech-en.txt", "w", encoding="utf-8")
+
+# line = sys.stdin.readlines()
+# newLine=""
+# for i in line:
+#     if i!="\n":
+#         newLine+=i
+
+# poseng=""
+# for pos in newLine.splitlines():
+#     if pos=="전체":
+#         poseng="whole"
+#     elif pos=="명사" or pos=="의존 명사":
+#         poseng="noun"
+#     elif pos=="대명사":
+#         poseng="pronoun"
+#     elif pos=="수사":
+#         poseng="number"
+#     elif pos=="조사":
+#         poseng="article"
+#     elif pos=="동사":
+#         poseng="verb"
+#     elif pos=="형용사" or pos=="관형사" or pos=="보조 형용사":
+#         poseng="adjective"
+#     elif pos=="부사":
+#         poseng="adverb"
+#     elif pos=="감탄사":
+#         poseng="interjection"
+#     elif pos=="접사":
+#         poseng=="affix"
+#     elif pos=="어미":
+#         poseng="ending"
+#     elif pos=="품사 없음":
+#         poseng=="no part of speech"
+#     print(poseng)
 
 ### Commented out to prevent overwriting of krtranswords.txt
 ### Scrape translated word
